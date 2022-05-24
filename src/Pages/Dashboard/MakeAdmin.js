468fb6d5dcfb6c5455edec49c4bdcc6b8d18c3ba
Rakibul Hasan Asif <47877578+asif2508@users.noreply.github.com>
2022-05-24 20:50:22 +0600

    make admin page created

--- src/Pages/Dashboard/MakeAdmin.js
+++ src/Pages/Dashboard/MakeAdmin.js
@@ -1,23 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Users from './Users';
 
 const MakeAdmin = () => {
+    const [users, setUsers] = useState([]);
+    useEffect(()=>{
+        fetch('http://localhost:5000/users',{
+            method: "GET",
+            headers:{
+                authorization: `Bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+        .then(res => res.json())
+        .then(data => setUsers(data))
+    },[users])
     return (
         <div className='border-2 border-solid-black'>
             <div className='xs:pl-3 sm:pl-3 lg:pl-12 bg-neutral flex justify-start flex-col pb-16 h-vh'>
                 <h1 className='text-3xl pt-2'>Manage Users and Admins</h1>
                 <div class="overflow-x-auto">
-                    <table class="table table-compact w-full">
+                    <table class="table table-zebra w-full">
                         <thead>
                             <tr>
                                 <th>#</th>
-                                <th>Name</th>
                                 <th>Email</th>
-                                <th>Previllage</th>
                                 <th>Make Admin</th>
+                                <th>Delete User</th>
                             </tr>
                         </thead>
                         <tbody>
-                        
+                        {
+                            users.map(user => <Users
+                            key={user._id}
+                            user={user}
+                            ></Users>)
+                        }
                     </tbody>
                 </table>
             </div>
--- src/Pages/Dashboard/Users.js
+++ src/Pages/Dashboard/Users.js
@@ -1,15 +1,23 @@
 import React from 'react';
 
-const Users = ({user}) => {
+const Users = ({index,user}) => {
+    const {email, role} = user;
+    const handleAdmin =()=>{
+        fetch(`http://localhost:5000/users/admin/${email}`,{
+            method: "PUT",
+            headers:{
+                authorization: `Bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+        .then(res => res.json())
+        .then(data => console.log(data))
+    }
     return (
         <tr>
-            <th>1</th>
-            <td>Cy Ganderton</td>
-            <td>Quality Control Specialist</td>
-            <td>Littel, Schaden and Vandervort</td>
-            <td>Canada</td>
-            <td>12/16/2020</td>
-            <td>Blue</td>
+            <th>#</th>
+            <td><p  className='text-accent'>{email}</p></td>
+            <td><button onClick={handleAdmin} class="btn btn-sm btn-secondary" disabled={role ==='admin' && 'disabled'}>Make Admin</button></td>
+            <td><button class="btn btn-sm btn-secondary">Delete</button></td>
         </tr>
     );
 };
