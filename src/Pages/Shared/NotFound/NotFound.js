c61c7742d33a48473a12a7f288cabce5e924efb9
Rakibul Hasan Asif <47877578+asif2508@users.noreply.github.com>
2022-05-28 18:23:36 +0600

    not found page created

--- src/Pages/Shared/NotFound/NotFound.js
+++ src/Pages/Shared/NotFound/NotFound.js
@@ -3,7 +3,9 @@ import React from 'react';
 const NotFound = () => {
     return (
         <div>
-            <h1>This is Not found</h1>
+            <div className='flex justify-center mt-16 mb-32'>
+            <img src='https://img.freepik.com/free-vector/404-error-with-person-looking-concept-illustration_114360-7912.jpg?t=st=1650269681~exp=1650270281~hmac=6a1b8bdff4491f9d3de4bcbf93e522c4c2d434856e4142c1d947f74db29cbd8d&w=740' fluid />
+            </div>
         </div>
     );
 };
